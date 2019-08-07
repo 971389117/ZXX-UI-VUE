ef7570771b96464a652f8113d9d8d7a890a79022
971389117 <971389117@qq.com>
2019-08-07 18:10:29 +0800

    完善 button

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="app">
-    <zButton></zButton>
+    <zButton>默认按钮</zButton>
+    <zButton type="primary">主要按钮</zButton>
+    <zButton type="success">成功按钮</zButton>
+    <zButton type="info">信息按钮</zButton>
+    <zButton type="warning">警告按钮</zButton>
+    <zButton type="danger">危险按钮</zButton>
+
     <zIcon></zIcon>
   </div>
 </template>
--- src/components/z-button.vue
+++ src/components/z-button.vue
@@ -3,19 +3,51 @@
 @create 2019-08-07-15:40
 -->
 <template>
-    <div class="z-button">
-        <button>按钮</button>
-    </div>
+    <button class="z-button" @click="handleClick"
+            :class="type">
+        <slot></slot>
+    </button>
 </template>
 
 <script>
     export default {
-        name: 'z-button'
+        name: 'z-button',
+        props: {
+            type: {
+                type: String,
+                default: 'default'
+            },
+            size:String,
+            icon:{
+                type:String,
+                default:'',
+            },
+            loading:Boolean,
+            disabled:Boolean,
+            plain:Boolean,
+            autofocus:Boolean,
+            round:Boolean,
+            circle:Boolean
+        },
     }
 </script>
 
 <style lang='scss' scoped>
     .z-button {
-
+        &.primary{
+            background-color: $color-primary;
+        }
+        &.success{
+            background-color: $color-success;
+        }
+        &.info{
+            background-color: $color-info;
+        }
+        &.warning{
+            background-color: $color-warning;
+        }
+        &.danger{
+            background-color: $color-danger;
+        }
     }
 </style>
